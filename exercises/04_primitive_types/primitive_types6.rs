7ac8054e928a42068f2c36c2e9719072abe8507a
locoy <mylocoy@gmail.com>
2025-11-13 15:52:26 +0800

    delete not

--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -12,6 +12,6 @@ mod tests {
         // and assign it to a variable called `second`.
         // let second = ???;
 
-        assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
+        assert_eq!(second, 2, "This is the 2nd number in the tuple!");
     }
 }
